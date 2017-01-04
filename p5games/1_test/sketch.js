98afa1f7e77d53e632d03fd69ec64f2ae7f49cce
ACharLuk <acharluk@gmail.com>
2017-01-04 15:54:51 +0100

    Update sketch.js

--- p5games/1_test/sketch.js
+++ p5games/1_test/sketch.js
@@ -1,14 +1,8 @@
-var i = 0;
 function setup() {
   createCanvas(1024, 600);
 }
 
 function draw() {
-  background(255,0,0, 0);
-  //ellipse(50, i, 80, 80);
+  background(255,0,0, 50);
   ellipse(mouseX, mouseY, 60, 60);
-  if (mouseIsPressed) {
-    i += mouseY / 100;
-  }
-  if (i > height) i = 0;
 }
