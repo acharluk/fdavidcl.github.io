0d418bd927af0fea49f142bfa57f9ef9eab3ad03
ACharLuk <acharluk@gmail.com>
2019-01-02 18:31:37 +0100

    Add ExplorerConcept page

---
+++ pages/explorerconcept/script.js
@@ -0,0 +1,40 @@
+function dragElement(elmnt) {
+    let b = document.getElementById('blur');
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    document.getElementById('titlebar').onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        b.style.backgroundPositionY = -(elmnt.offsetTop - pos2) + 240 + 'px';
+        b.style.backgroundPositionX = -(elmnt.offsetLeft - pos1) + 400 + 'px';
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
+let w = document.getElementById('window');
+dragElement(w);
